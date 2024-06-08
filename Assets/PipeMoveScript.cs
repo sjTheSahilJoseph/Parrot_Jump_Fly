da0385be71fec595338b30151b275466973fe8ad
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-06-08 14:52:54 +0500

    pipe moving

---
+++ Assets/PipeMoveScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour
+{
+
+	public float move_speed = 5;
+	
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * move_speed) * Time.deltaTime;
+    }
+}
